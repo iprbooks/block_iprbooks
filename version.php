7e7be1e45588712020eb67db4b3fce877565f510
andrey <andrey.cherkasov93@gmail.com>
2020-04-17 14:32:55 +0400

    fix for publish

--- version.php
+++ version.php
@@ -1,4 +1,6 @@
 <?php
 $plugin->component = 'block_iprbooks';
-$plugin->version = 2020041600;
+$plugin->version = 2020041700;
 $plugin->requires = 2013051401;
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = 'v1.2';
